6ef1d560908d166f9fbdf1acdeaa1668ae089c8e
47rooks <47rooks@gmail.com>
2024-05-11 20:25:45 -0700

    fix release.bat to include user readme, exclude haxelib zip

--- release.bat
+++ release.bat
@@ -3,7 +3,7 @@ del tiny-profiler.zip
     -i!tinyprofiler\* ^
     -i!buildsupport\* ^
     -i!ndll\*^
-    -xr!*.md ^
+    -xr!**\*.md ^
     -i!lib\tpLib\timing.h ^
     -i!include.xml ^
     -i!ATTRIBUTIONS.md -i!CHANGELOG.md -i!LICENSE -i!README.md -i!haxelib.json 
